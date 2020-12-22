6687a196400fe1c063194ba7d1170945dfbd3932
bobanderic2000 <bobanderic2000@gmail.com>
2020-12-22 20:11:14 +0800

    warm up done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,20 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// HW
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+// https://www.youtube.com/watch?v=bn9arlhfaXc
+// 小胖有加下面這些，但我不知道這在幹嘛，好像也不需要
+// document.addEventListener("turbolinks:load", () =>{
+//   $('[data-toggle="tooltip"]').tooltip()
+//   $('[data-toggle="popover"]').popover()
+// })
+
+// https://www.youtube.com/watch?v=cWkISBYM_0g
+// 另一個人講到可能要加這個，但我也不知道這是做什麼用的
+// global.$ = jQuery;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
